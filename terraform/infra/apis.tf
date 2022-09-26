af9a79b147afeb195bd56a4b66ba211936f54cdf
Marcin Kubica <marcin.kubica@sap.com>
2022-09-26 13:21:23 +0100

    no need var for api list

--- terraform/infra/apis.tf
+++ terraform/infra/apis.tf
@@ -1,17 +1,12 @@
 # Set apis to not disable in case we issue `terraform destroy`
-variable "apis" {
-  type = list
-  default = [
+resource "google_project_service" "apis" {
+  for_each = toset([
     "cloudresourcemanager.googleapis.com",
     "secretmanager.googleapis.com",
     "sqladmin.googleapis.com",
     "container.googleapis.com",
     "iam.googleapis.com"
-  ]
-}
-
-resource "google_project_service" "apis" {
-  for_each = toset(var.apis)
+  ])
   service = each.key
   project = var.project
   disable_on_destroy = false
