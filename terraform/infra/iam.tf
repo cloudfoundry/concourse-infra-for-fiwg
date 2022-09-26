d5cfcab75ce61611c075759a3448b691b870c784
Marcin Kubica <marcin.kubica@sap.com>
2022-09-26 12:00:10 +0100

    add cnrm account, leave concourse account out for now

--- terraform/infra/iam.tf
+++ terraform/infra/iam.tf
@@ -5,3 +5,29 @@ resource "google_service_account" "autoscaler_deployer" {
   display_name  = "autoscaler-deployer"
   project       = var.project
 }
+
+# resource "google_service_account" "concourse" {
+#   account_id    = "concourse"
+#   description   = "concourse deployment on wg-ci gke"
+#   disabled      = "false"
+#  project       = var.project
+# }
+
+resource "google_service_account" "cnrm-system" {
+  account_id    = "cnrm-system"
+  description   = "Config Connector account for wg-ci GKE"
+  disabled      = "false"
+  project       = var.project
+}
+
+resource "google_project_iam_binding" "project-binding" {
+
+  for_each = toset([
+    "roles/iam.serviceAccountUser",
+    "roles/iam.workloadIdentityUser"
+    ])
+  role               = each.key
+
+  members = [ "serviceAccount:${var.project}.svc.id.goog[${google_service_account.cnrm-system.name}/cnrm-controller-manager"]
+  project       = var.project
+}
