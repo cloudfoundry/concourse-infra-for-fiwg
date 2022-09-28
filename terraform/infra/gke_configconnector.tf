1be74724e1d663da10fe66fed4111b4f930602f0
Marcin Kubica <marcin.kubica@sap.com>
2022-09-28 10:26:16 +0100

    fix account def for ConfigConnector

--- terraform/infra/gke_configconnector.tf
+++ terraform/infra/gke_configconnector.tf
@@ -1,25 +1,16 @@
-# resource "kubernetes_manifest" "cnrm" {
-#     manifest = {
-#         apiVersion = "core.cnrm.cloud.google.com/v1beta1"
-#         kind = "ConfigConnector"
-#         metadata = {
-#             "name" = "configconnector.core.cnrm.cloud.google.com"
-#         }
-#         spec = {
-#             mode = "cluster"
-#             googleServiceAccount = "${google_service_account.cnrm-system.name}"
+resource "kubernetes_manifest" "cnrm" {
+    manifest = {
+        apiVersion = "core.cnrm.cloud.google.com/v1beta1"
+        kind = "ConfigConnector"
+        metadata = {
+            "name" = "configconnector.core.cnrm.cloud.google.com"
+        }
+        spec = {
+            mode = "cluster"
+            googleServiceAccount = "${google_service_account.cnrm-system.email}"
 
-#         }
-#     }
+        }
+    }
   
-# }
-
-
-# apiVersion: core.cnrm.cloud.google.com/v1beta1
-# kind: ConfigConnector
-# metadata:
-#   name: configconnector.core.cnrm.cloud.google.com
-# spec:
-#  mode: cluster
-#  googleServiceAccount: #@ "cnrm-system@" + data.values.google.project_id + ".iam.gserviceaccount.com"
+}
 
