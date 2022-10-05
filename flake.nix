396d6d0e4bf259bb9ad8c00e836d20845ad8ea7f
Jörg Weisbarth <joerg.weisbarth@sap.com>
2022-10-05 14:43:56 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Dependencies of stemcell infrastructure";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              kapp
+              terraform
+              terragrunt
+              # vendir
+              ytt
+            ];
+          };
+      });
+  };
+}
