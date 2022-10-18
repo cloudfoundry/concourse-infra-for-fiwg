efc613229324ccdbf64335a5afd7d70e6746dfc9
Marcin Kubica <marcin.kubica@sap.com>
2022-10-18 16:29:53 +0100

    fix router project

--- terraform/concourse-infra/routers.tf
+++ terraform/concourse-infra/routers.tf
@@ -11,6 +11,7 @@ resource "google_compute_router_nat" "nat_config" {
   name                                = "nat-config"
   router                              = google_compute_router.nat_router.name
   region                              = google_compute_router.nat_router.region
+  project			      = var.project
   nat_ip_allocate_option              = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat  = "ALL_SUBNETWORKS_ALL_IP_RANGES"
   min_ports_per_vm                    = "4095"
