9f9d61130a751dd14523a77d17799c6134756a25
Joseph Palermo <jpalermo@pivotal.io>
2023-11-24 10:31:54 -0800

    Guard against fly version is out of sync with the target errors that can happen after the upgrader instance has started

--- ci/tasks/deploy-concourse/wait-for-concourse-drain.sh
+++ ci/tasks/deploy-concourse/wait-for-concourse-drain.sh
@@ -6,12 +6,16 @@ chmod +x /usr/local/bin/fly
 
 fly --target concourse login --concourse-url="${CONCOURSE_URL}" --username="${CONCOURSE_USERNAME}" --password="${CONCOURSE_PASSWORD}"
 
+# Some requests may fail because fly is the incorrect version for the new web instance
+set +e
 while true; do
   running_job_count=$(fly --target concourse builds --count=1000 --json | jq '[.[] | select(.status == "started") | select(.job_name != env.IGNORE_JOB)] | length')
-  if [  "${running_job_count}" -eq 0 ]; then
-    echo "No more jobs running"
-    break
+  if [ $? -eq 0 ]; then
+    if [  "${running_job_count}" -eq 0 ]; then
+      echo "No more jobs running"
+      break
+    fi
+    echo "Waiting on ${running_job_count} jobs to finish"
+    sleep 60
   fi
-  echo "Waiting on ${running_job_count} jobs to finish"
-  sleep 60
 done
